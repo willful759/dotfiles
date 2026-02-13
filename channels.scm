38cad57673843e2818217a8f6328e4d70b9354f5
willful759 <ivanpineda24061@gmail.com>
2026-02-12 22:48:58 -0600

    fixed updated guix channel

--- channels.scm
+++ channels.scm
@@ -1,6 +1,6 @@
 (list (channel
         (name 'guix)
-        (url "https://codeberg.org/git/guix.git"))
+        (url "https://codeberg.org/guix/guix.git"))
       (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")))
